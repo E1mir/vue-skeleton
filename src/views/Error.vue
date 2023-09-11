839a9c42e0a5aac0cc5405d8282968f2e7a92529
Elmir Ismayilzada <mrelmirismayilzade@gmail.com>
2023-09-11 13:40:56 +0200

    feat: add Error page

---
+++ src/views/Error.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="error">
+    <div>Page not found</div>
+    <router-link to="/">
+      Back to Home
+    </router-link>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.error {
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+}
+</style>
