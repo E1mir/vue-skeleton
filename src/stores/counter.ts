bb761db94b31a3b56780504207464204314d4861
Elmir Ismayilzada <mrelmirismayilzade@gmail.com>
2023-09-11 14:21:28 +0200

    feat: add counter store

---
+++ src/stores/counter.ts
@@ -0,0 +1,15 @@
+import { acceptHMRUpdate, defineStore } from 'pinia'
+import { ref } from 'vue'
+
+export const useCounterStore = defineStore('counter', () => {
+  const count = ref(0)
+  function increment() {
+    count.value++
+  }
+
+  return { count, increment }
+})
+
+if (import.meta.hot) {
+  import.meta.hot.accept(acceptHMRUpdate(useCounterStore, import.meta.hot))
+}
