48b24c72b502573d23b3d1bc9b78f502f86f8880
Elmir Ismayilzada <mrelmirismayilzade@gmail.com>
2023-09-11 14:21:12 +0200

    feat: configure pinia

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import { router } from './router'
 
 import App from './App.vue'
 
 import './styles/style.scss'
 
+const pinia = createPinia()
 const app = createApp(App)
+
 app.use(router)
+app.use(pinia)
 
 app.mount('#app')
