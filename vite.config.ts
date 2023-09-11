bc5f67f13d1fed3f9e3de23420b17b8f5cb39434
Elmir Ismayilzada <mrelmirismayilzade@gmail.com>
2023-09-11 11:28:51 +0200

    feat: update vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import path from 'path'
+
 // https://vitejs.dev/config/
 export default defineConfig({
+  // For git-pages, gets package name from package.json
+  base: process.env.NODE_ENV === 'production' ? process.env.npm_package_name : '/',
   plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: `@import "@/styles/_variables.scss";`
+      }
+    }
+  }
 })
